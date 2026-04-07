d5fd291f5b35b7eac86ea0b0cb84cdf3de32c62e
Luke Iannucci <luke.iannucci@outlook.com>
2026-04-07 15:00:01 -0400

    start

--- espresso_e2e/e2e_utils.go
+++ espresso_e2e/e2e_utils.go
@@ -7,9 +7,11 @@ import (
 	"fmt"
 	"io"
 	"log/slog"
+	"math/big"
 	"net/http"
 	"os/exec"
 	"proxy/proxy"
+	verifier "proxy/verifier/op"
 	"strconv"
 	"strings"
 	"sync"
@@ -18,13 +20,89 @@ import (
 
 	espressostore "proxy/store"
 
+	espressoClient "github.com/EspressoSystems/espresso-network/sdks/go/client"
+	opStreamer "github.com/EspressoSystems/espresso-streamers/op"
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/ethclient"
 	"github.com/ethereum/go-ethereum/log"
 	"github.com/stretchr/testify/require"
 )
 
+// For e2e tests we are using the mock client as there currently is an issue with light client in espresso dev node
+// Eventually we will fix it and remove this.
+type mockLightClient struct {
+	client *espressoClient.Client
+	last   uint64
+}
+
+func (m *mockLightClient) FinalizedState(_ *bind.CallOpts) (opStreamer.FinalizedState, error) {
+	current, err := m.client.FetchLatestBlockHeight(context.Background())
+	result := m.last
+	if err == nil {
+		// Make sure finalized state is back enough blocks
+		m.last = 0
+		if current > finalizedBlocks {
+			m.last = current - finalizedBlocks
+		}
+	}
+	return opStreamer.FinalizedState{
+		BlockHeight:   result,
+		ViewNum:       0,
+		BlockCommRoot: big.NewInt(0),
+	}, nil
+}
+
+const (
+	rollupWorkingDir = "./op"
+	l1GethURL        = "http://127.0.0.1:8545"
+	espressoURL      = "http://127.0.0.1:24000"
+	opGethSeqURL     = "http://127.0.0.1:8546"
+	opGethFullNode   = "http://127.0.0.1:8555"
+	opNodeSeqURL     = "http://127.0.0.1:9545"
+	opNodeFullNode   = "http://127.0.0.1:9548"
+	mockBeaconURL    = "http://127.0.0.1:5052"
+	p2pAttackUrl     = "http://127.0.0.1:8560"
+	L2_CHAIN_ID      = 22266222
+	espressoTag      = "espresso"
+	finalizedBlocks  = 60
+)
+
+func startVerifier(ctx context.Context, t *testing.T, logger log.Logger, store *espressostore.EspressoStore) *verifier.OPEspressoBatchVerifier {
+	t.Helper()
+	l1Client, err := ethclient.DialContext(ctx, l1GethURL)
+	if err != nil {
+		logger.Crit("failed to create L1 client", "error", err)
+	}
+	v := verifier.NewOPEspressoBatchVerifier(ctx, logger, store,
+		l1Client,
+		&mockLightClient{client: espressoClient.NewClient(espressoURL)},
+		&verifier.OPEspressoBatchVerifierConfig{
+			FullNodeExecutionRPC:      opGethFullNode,
+			FullNodeConsensusRPC:      opNodeFullNode,
+			VerificationInterval:      time.Second,
+			QueryServiceURL:           espressoURL,
+			BatcherAddress:            "0x976EA74026E726554dB657fA54763abd0C3a0aa9",
+			BatchAuthenticatorAddress: "0x4826533b4897376654bb4d4ad88b7fafd0c98528",
+		},
+	)
+	v.Start(ctx)
+	return v
+}
+
 func runDockerCompose(workingDir string, services ...string) func() {
+	return runDockerComposeFile(workingDir, "", services...)
+}
+
+func runDockerComposeFile(workingDir string, composeFile string, services ...string) func() {
+	fileArgs := []string{}
+	if composeFile != "" {
+		fileArgs = []string{"-f", composeFile}
+	}
+
 	shutdown := func() {
-		p := exec.Command("docker", "compose", "down", "--volumes", "--remove-orphans")
+		args := append([]string{"compose"}, fileArgs...)
+		args = append(args, "down", "--volumes", "--remove-orphans")
+		p := exec.Command("docker", args...)
 		p.Dir = workingDir
 		if out, err := p.CombinedOutput(); err != nil {
 			log.Error("docker compose down failed", "error", err, "output", string(out))
@@ -33,7 +111,8 @@ func runDockerCompose(workingDir string, services ...string) func() {
 
 	shutdown()
 
-	invocation := []string{"compose", "up", "-d", "--pull", "always"}
+	invocation := append([]string{"compose"}, fileArgs...)
+	invocation = append(invocation, "up", "-d", "--pull", "always")
 	invocation = append(invocation, services...)
 	cmd := exec.Command("docker", invocation...)
 	cmd.Dir = workingDir
