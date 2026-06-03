783245c0e3d213691e632a79108d6f36a74bd272
Theodore Schnepper <tschnepper@espressosys.com>
2026-06-03 10:29:10 -0600

    Modify auto closer to be defered
    The behavior of the Auto Body Closer matched this updated implemention
    expect in a single case.  That being a `panic`.  If a request panics,
    the body would not be closed.
    
    As a result, we add the `Close` within a `defer` call to ensure that it
    still gets ran.
    
    Additionally, the comments have been updated to cite the source of the
    criticism for its existence.

--- http/body_auto_closer.go
+++ http/body_auto_closer.go
@@ -13,19 +13,28 @@ type httpAutoBodyCloserMiddleware struct {
 	logger  log.Logger
 }
 
+// ServeHttp implements http.Handler
+//
+// This method automatically closes the Body after the handler has completed
+// the request.
 func (m *httpAutoBodyCloserMiddleware) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	m.handler.ServeHTTP(w, r)
-	// Body is guaranteed not to be `nil` based on documentation annotation
-	// on the `Body` field within `http.Request`.
-	if r.Body == nil {
-		return
-	}
+	go (func() {
+		// Body is guaranteed not to be [nil] for ServeHTTP based on
+		// documentation annotation on the `Body` field within [http.Request]:
+		// https://cs.opensource.google/go/go/+/refs/tags/go1.25.0:src/net/http/request.go;l=180;drc=b2819d13dbe19343426e688da4ddfeb57c8589fc
+		if r.Body == nil {
+			return
+		}
 
-	// Additionally, according to the documentation some more, calling `Close`
-	// on the `request.Body` is not necessary.
-	if err := r.Body.Close(); err != nil {
-		m.logger.Warn("failed to close request body", "error", err)
-	}
+		// Additionally, according to the documentation some more, calling [Close]
+		// on the [request.Body] is not necessary:
+		// https://cs.opensource.google/go/go/+/refs/tags/go1.25.0:src/net/http/request.go;l=182-183;drc=b2819d13dbe19343426e688da4ddfeb57c8589fc
+		if err := r.Body.Close(); err != nil {
+			m.logger.Warn("failed to close request body", "error", err)
+		}
+	})()
+
+	m.handler.ServeHTTP(w, r)
 }
 
 // AutoBodyCloserMiddleware is a middleware that automatically closes the
