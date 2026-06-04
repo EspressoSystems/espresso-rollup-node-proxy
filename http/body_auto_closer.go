509de57143a0d3427c3eae4fd5aad7c419e07d8f
Theodore Schnepper <tschnepper@espressosys.com>
2026-06-04 09:04:16 -0600

    Replace goroutine with defer
    Based on feedback from Gemini:
    https://github.com/EspressoSystems/espresso-rollup-node-proxy/pull/48#discussion_r3356867497
    
    This was meant to be a `defer` but was accidentally utilized as a `go
    routine` call.

--- http/body_auto_closer.go
+++ http/body_auto_closer.go
@@ -18,7 +18,7 @@ type httpAutoBodyCloserMiddleware struct {
 // This method automatically closes the Body after the handler has completed
 // the request.
 func (m *httpAutoBodyCloserMiddleware) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	go (func() {
+	defer func() {
 		// Body is guaranteed not to be [nil] for ServeHTTP based on
 		// documentation annotation on the `Body` field within [http.Request]:
 		// https://cs.opensource.google/go/go/+/refs/tags/go1.25.0:src/net/http/request.go;l=180;drc=b2819d13dbe19343426e688da4ddfeb57c8589fc
@@ -32,7 +32,7 @@ func (m *httpAutoBodyCloserMiddleware) ServeHTTP(w http.ResponseWriter, r *http.
 		if err := r.Body.Close(); err != nil {
 			m.logger.Warn("failed to close request body", "error", err)
 		}
-	})()
+	}()
 
 	m.handler.ServeHTTP(w, r)
 }
