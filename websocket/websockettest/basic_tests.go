4491f32b1030077400966ba7dd925bd75a2e004c
Theodore Schnepper <tschnepper@espressosys.com>
2026-06-08 11:25:59 -0600

    Add websockettest package
    Inspired by the `net/http/httptest` package, we provide some utilities
    for quickly and easily spinning up a test server to easily provide and
    spin up a WebSocket server for testing purposes.
    
    This also serves as a placeholder for future utility based on the
    websocket package.
    
    Additionally, we define a basic test suite for testing correctness of
    the Websocket abstraction implementation.

---
+++ websocket/websockettest/basic_tests.go
@@ -0,0 +1,213 @@
+package websockettest
+
+import (
+	"net/http"
+	"testing"
+	"time"
+
+	"proxy/websocket"
+
+	"github.com/stretchr/testify/require"
+)
+
+type BasicSuite struct {
+	websocket.Upgrader
+	websocket.Dialer
+}
+
+func NewBasicSuite(upgrader websocket.Upgrader, dialer websocket.Dialer) *BasicSuite {
+	return &BasicSuite{
+		Upgrader: upgrader,
+		Dialer:   dialer,
+	}
+}
+
+// RunBasicServerCloseTest tests that a connection of the WebSocket will
+// be closed successfully, and that the reported reason and status code will
+// be returned successfully when everything is setup correctly.
+func (b *BasicSuite) RunBasicServerCloseTest(t *testing.T, newServer ServerCreator, status websocket.Status, message string) {
+	// Setup
+	require := require.New(t)
+	ctx := t.Context()
+
+	// This handler will immediately close any connection found
+	handler := WebSocketHandlerFunc(func(conn websocket.Conn, err error) {
+		// We should not have an issue connecting to the server
+		require.NoError(err)
+
+		// Let's try to close the connection
+		require.NoError(conn.Close(status, message))
+
+		time.Sleep(time.Millisecond * 100)
+
+		_, _, err = conn.Read(ctx)
+		require.Error(err)
+	})
+
+	server := newServer(b, handler)
+	defer server.Close()
+
+	// Connect to the Server
+	conn, response, err := server.Connect(ctx, b)
+
+	require.NoError(err)
+	require.Equal(response.StatusCode, http.StatusSwitchingProtocols)
+
+	// Try to read something from the connection, to force the Close connection
+	_, _, err = conn.Read(ctx)
+	require.Error(err)
+
+	closeErr, ok := conn.IsCloseError(err)
+	require.True(ok, "error should be a close error")
+	require.Equal(closeErr.Status, status)
+	require.Equal(closeErr.Reason, message)
+
+	require.Error(conn.Write(ctx, websocket.MessageTypeText, []byte("hello there")))
+}
+
+// RunBasicClientCloseTest tests that a connection of the WebSocket will
+// setup and disconnect correctly when the client sends a Close request.
+func (b *BasicSuite) RunBasicClientCloseTest(t *testing.T, newServer ServerCreator, status websocket.Status, message string) {
+	// Setup
+	require := require.New(t)
+	ctx := t.Context()
+
+	// This handler will immediately close any connection found
+	handler := WebSocketHandlerFunc(func(conn websocket.Conn, err error) {
+		// We should not have an issue connecting to the server
+		require.NoError(err)
+
+		_, _, err = conn.Read(ctx)
+		require.Error(err)
+		closeErr, ok := conn.IsCloseError(err)
+		require.True(ok, "err should be a close error")
+		require.Equal(closeErr.Status, status)
+		require.Equal(closeErr.Reason, message)
+
+		// 		require.NoError(conn.Close(status, message))
+	})
+
+	// Start the WebSocket server with a handler that will close the
+	server := NewServer(b, handler)
+	// connection immediately
+	defer server.Close()
+
+	// Connect to the Server
+	conn, response, err := server.Connect(ctx, b)
+
+	require.NoError(err)
+	require.Equal(response.StatusCode, http.StatusSwitchingProtocols)
+
+	// Try to read something from the connection, to force the Close connection
+	require.NoError(conn.Close(status, message))
+
+	_, _, err = conn.Read(ctx)
+	require.Error(err)
+}
+
+// RunBasicWriteMessageTest tests that a message
+// can be written to the WebSocket connection successfully, and that the
+// message is received on the other end of the connection.
+func (b *BasicSuite) RunBasicWriteMessageTest(t *testing.T, newServer ServerCreator, expectedMessageType websocket.MessageType, expectedMessage []byte) {
+	// Setup
+	require := require.New(t)
+	ctx := t.Context()
+
+	handler := WebSocketHandlerFunc(func(conn websocket.Conn, err error) {
+		require.NoError(err)
+
+		// Ensure that we can read a message
+		messageType, message, err := conn.Read(ctx)
+		require.NoError(err)
+
+		require.Equal(expectedMessageType, messageType)
+		require.Equal(expectedMessage, message)
+	})
+
+	// Start the WebSocket server with a handler that will read a message and
+	// and verify that it matches the expected message and message type
+	server := NewServer(b, handler)
+	defer server.Close()
+
+	// Connect to the Server
+	conn, response, err := server.Connect(ctx, b)
+
+	require.NoError(err)
+	require.Equal(response.StatusCode, http.StatusSwitchingProtocols)
+
+	// Let's write something
+	require.NoError(conn.Write(ctx, expectedMessageType, []byte(expectedMessage)))
+
+	require.NoError(conn.Close(websocket.StatusNormalClosure, "goodbye"))
+}
+
+// RunBasicReadMessageTest tests that a message
+// server side will write the message successfully.
+// can be read from the WebSocket connection successfully, and that the
+func (b *BasicSuite) RunBasicReadMessageTest(t *testing.T, newServer ServerCreator, expectedMessageType websocket.MessageType, expectedMessage []byte) {
+	// Setup
+	require := require.New(t)
+	ctx := t.Context()
+
+	handler := WebSocketHandlerFunc(func(conn websocket.Conn, err error) {
+		require.NoError(err)
+
+		// Ensure that we can write a message
+		require.NoError(conn.Write(ctx, expectedMessageType, []byte(expectedMessage)))
+
+		require.NoError(conn.Close(websocket.StatusNormalClosure, "goodbye"))
+	})
+
+	// Start the WebSocket server with a handler that will read a message and
+	// and verify that it matches the expected message and message type
+	server := NewServer(b, handler)
+	defer server.Close()
+
+	// Connect to the Server
+	conn, response, err := server.Connect(ctx, b)
+
+	require.NoError(err)
+	require.Equal(response.StatusCode, http.StatusSwitchingProtocols)
+
+	// Let's read something
+	messageType, message, err := conn.Read(ctx)
+	require.NoError(err)
+	require.Equal(expectedMessageType, messageType)
+	require.Equal(expectedMessage, message)
+}
+
+// CreateBasicServerCloseTest creates a test function that will perform the
+// [BasicSuite.RunBasicServerCloseTest] test with the provided status and
+// reason.
+func (b *BasicSuite) CreateBasicServerCloseTest(newServer ServerCreator, status websocket.Status, reason string) func(t *testing.T) {
+	return func(t *testing.T) {
+		b.RunBasicServerCloseTest(t, newServer, status, reason)
+	}
+}
+
+// CreateBasicClientCloseTest creates a test function that will perform the
+// [BasicSuite.RunBasicClientCloseTest] test with the provided status and
+// reason.
+func (b *BasicSuite) CreateBasicClientCloseTest(newServer ServerCreator, status websocket.Status, reason string) func(t *testing.T) {
+	return func(t *testing.T) {
+		b.RunBasicClientCloseTest(t, newServer, status, reason)
+	}
+}
+
+// CreateBasicReadsMessagesTest creates a test function that will perform the
+// [BasicSuite.RunBasicReadMessageTest] test with the provided message type
+// and message.
+func (b *BasicSuite) CreateBasicWriteMessageTest(newServer ServerCreator, messageType websocket.MessageType, message []byte) func(t *testing.T) {
+	return func(t *testing.T) {
+		b.RunBasicWriteMessageTest(t, newServer, messageType, message)
+	}
+}
+
+// CreateBasicReadsMessagesTest creates a test function that will perform the
+// [BasicSuite.RunBasicReadMessageTest] test with the provided message type
+// and message.
+func (b *BasicSuite) CreateBasicReadMessageTest(newServer ServerCreator, messageType websocket.MessageType, message []byte) func(t *testing.T) {
+	return func(t *testing.T) {
+		b.RunBasicReadMessageTest(t, newServer, messageType, message)
+	}
+}
---
+++ websocket/websockettest/test_server.go
@@ -0,0 +1,102 @@
+package websockettest
+
+import (
+	"context"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+
+	"proxy/websocket"
+)
+
+// WebSocketHandler is an interface that represents a handler for WebSocket
+// upgraded requests.
+type WebSocketHandler interface {
+	// ServeWebSocket is called with the results of performing a call on
+	// [websocket.Upgrader.Upgrade]. The error may be populated, and if it
+	// is, will mean that the provided [websocket.Conn] is not valid,
+	// and should not be used.
+	ServeWebSocket(conn websocket.Conn, err error)
+}
+
+// WebSocketHandlerFunc is a helper type that allows us to use a simple
+// function as a [WebSocketHandler].
+type WebSocketHandlerFunc func(websocket.Conn, error)
+
+// Compile-time interface adherence assertion.
+var _ WebSocketHandler = WebSocketHandlerFunc(nil)
+
+// ServeWebSocket implements [WebSocketHandler] by calling the underlying
+// funtion.
+func (f WebSocketHandlerFunc) ServeWebSocket(conn websocket.Conn, err error) {
+	f(conn, err)
+}
+
+// Server represents a test WebSocket server, which can be used to test
+// WebSocket clients.
+type Server struct {
+	*httptest.Server
+	Handler  WebSocketHandler
+	Upgrader websocket.Upgrader
+}
+
+func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	conn, err := s.Upgrader.Upgrade(w, r)
+	s.Handler.ServeWebSocket(conn, err)
+}
+
+// WSURL returns the WebSocket URL for the test server, which can be used to
+// connect to the server using a WebSocket client.
+func (s *Server) WSURL() *url.URL {
+	return &url.URL{
+		Scheme: "ws",
+		Host:   s.Listener.Addr().String(),
+	}
+}
+
+// Connect connects to the test server using the provided dialer, and
+// returns thew new WebSocket connection.
+func (s *Server) Connect(ctx context.Context, dialer websocket.Dialer) (websocket.Conn, *http.Response, error) {
+	return dialer.Dial(ctx, s.WSURL().String())
+}
+
+// NewServer creates a new test WebSocket server with the given upgrader
+// and handler.
+//
+// The Caller **SHOULD** call the [Server.Close] method when finished, to shut
+// down the server.
+func NewServer(upgrader websocket.Upgrader, handler WebSocketHandler) *Server {
+	server := NewUnstartedServer(upgrader, handler)
+	server.Start()
+	return server
+}
+
+// NewUnstartedServer creates a new test WebSocket server with the given
+// [websocket.Upgrader] and [WebSocketHandler], but does not start the server.
+func NewUnstartedServer(upgrader websocket.Upgrader, handler WebSocketHandler) *Server {
+	server := &Server{
+		Upgrader: upgrader,
+		Handler:  handler,
+	}
+
+	server.Server = httptest.NewUnstartedServer(server)
+	return server
+}
+
+// TestServer represents a test WebSocket server, which can be used to test
+// specific behavior with WebSocket servers.
+type TestServer interface {
+	// Connect connects to the test server using the provided dialer.
+	Connect(ctx context.Context, dialer websocket.Dialer) (websocket.Conn, *http.Response, error)
+
+	Close()
+}
+
+// TestServerCreator creates a new test server with the given
+// [websocket.Upgrader] and [WebSocketHandler].
+func TestServerCreator(upgrader websocket.Upgrader, handler WebSocketHandler) TestServer {
+	return NewServer(upgrader, handler)
+}
+
+// ServerCreator represents a function that can be used to create a new test
+type ServerCreator func(upgrader websocket.Upgrader, handler WebSocketHandler) TestServer
