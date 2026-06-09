3fdf9e87f1060132b8843fc07eb26c1b9b314909
Theodore Schnepper <tschnepper@espressosys.com>
2026-06-09 13:03:26 -0600

    Add a test for websocket header pruning
    The websocket header cloning and pruning for proxy should have, at the
    very least, a basic test to verify that it behaves as expected.

---
+++ websocket/header_test.go
@@ -0,0 +1,34 @@
+package websocket_test
+
+import (
+	"net/http"
+	"testing"
+
+	"github.com/EspressoSystems/espresso-rollup-node-proxy/websocket"
+	"github.com/stretchr/testify/require"
+)
+
+// TestCloneRequestHeadersForProxy tests that the CloneRequestHeadersForProxy
+// function that ensures that we omit headers for proxy requests that impose
+// restrictions on the WebSocket connection establishment efforts.
+func TestCloneRequestHeadersForProxy(t *testing.T) {
+	require := require.New(t)
+	header := http.Header{
+		websocket.HeaderOrigin:                 []string{"example.com"},
+		websocket.HeaderSecWebSocketKey:        []string{"dGhlIHNhbXBsZSBub25jZQ=="},
+		websocket.HeaderSecWebSocketVersion:    []string{"13"},
+		websocket.HeaderSecWebSocketProtocol:   []string{"chat, superchat"},
+		websocket.HeaderSecWebSocketExtensions: []string{"permessage-deflate; client_max_window_bits"},
+		websocket.HeaderUpgrade:                []string{"websocket"},
+
+		"Test": []string{"value"},
+	}
+
+	cloned := websocket.CloneRequestHeadersForProxy(header)
+
+	require.Equal(
+		http.Header{
+			"Test": []string{"value"},
+		}, cloned,
+	)
+}
